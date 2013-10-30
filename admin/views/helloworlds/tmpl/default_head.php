4ac8a201d828f76cc581fd0ab4ee06f1eddd8f31
Dmitry Rekun <bzzjuka@gmail.com>
2013-10-30 19:35:45 +0200

    Fix closing tag

--- admin/views/helloworlds/tmpl/default_head.php
+++ admin/views/helloworlds/tmpl/default_head.php
@@ -4,7 +4,7 @@ defined('_JEXEC') or die;
 ?>
 <tr>
 	<th width="1%">
-		<input type="checkbox" name="checkall-toggle" value="" title="<?php echo JText::_('JGLOBAL_CHECK_ALL'); ?>" onclick="Joomla.checkAll(this)"
+		<input type="checkbox" name="checkall-toggle" value="" title="<?php echo JText::_('JGLOBAL_CHECK_ALL'); ?>" onclick="Joomla.checkAll(this)" />
 	</th>
 	<th>
 		<?php echo JText::_('COM_HELLOWORLD_HELLOWORLD_HEADING_GREETING'); ?>
